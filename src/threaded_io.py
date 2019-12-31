f694a2753dc4e2f00f35cb4498a67c97ba99f21a
Iyed Ghedamsi <iyed.ghdemsi@gmail.com>
2019-12-31 23:44:38 +0100

    show timestamp and chunking

--- src/threaded_io.py
+++ src/threaded_io.py
@@ -24,7 +24,13 @@ READER_RECEIVED_FRAME_EVT = signal("READER_RECEIVED_FRAME_EVT")
 # TODO: implement __iter__
 class DownloaderThread(threading.Thread):
     def __init__(
-        self, youtube_link, sink_queue, name=cute_name, frame_skip=0, daemon=True,
+        self,
+        youtube_link,
+        sink_queue,
+        name=cute_name(),
+        frame_skip=0,
+        daemon=True,
+        chunk_size=120,
     ):
         super().__init__()
         self.youtube_link = youtube_link
@@ -34,6 +40,7 @@ class DownloaderThread(threading.Thread):
         self.killer_queue = queue.Queue(1)
         self.daemon = daemon
         self.name = name
+        self.chunk_size = chunk_size
         DOWNLOADER_STOP_EVT.connect(self.on_kill)
 
     def run(self):
@@ -41,21 +48,25 @@ class DownloaderThread(threading.Thread):
         video_stream_url = pafy.new(self.youtube_link).getbest().url
         capture = cv2.VideoCapture(video_stream_url)
         while True:
+            buffer = []
             if not self.killer_queue.empty():
                 if self.killer_queue.get_nowait() == SENTINEL:
                     DOWNLOADER_EO_STREAM_EVT.send(self)
                     break
-            success, frame = capture.read()
-            if not success:
-                self.sink_queue.put(SENTINEL)
-                break
-            else:
-                self.current_frame += 1
-            if (
-                self.current_frame % (self.frame_skip + 1) == 0
-                or self.current_frame == 1
-            ):
-                self.sink_queue.put(frame.tolist())
+            if len(buffer) <= self.chunk_size:
+                success, frame = capture.read()
+                ts = capture.get(cv2.CAP_PROP_POS_MSEC)
+                if not success:
+                    self.sink_queue.put(SENTINEL)
+                    break
+                else:
+                    self.current_frame += 1
+                if (
+                    self.current_frame % (self.frame_skip + 1) == 0
+                    or self.current_frame == 1
+                ):
+                    buffer.append({"frame": frame, "timestamp": ts})
+            self.sink_queue.put(buffer)
 
     def on_kill(self, sender):
         logger.debug(f"{DOWNLOADER_STOP_EVT} received")
--- src/ui.py
+++ src/ui.py
@@ -1,3 +1,6 @@
+import cProfile
+import io
+import pstats
 import queue
 import threading
 import time
@@ -7,10 +10,10 @@ import streamlit as st
 from loguru import logger
 
 from threaded_io import (
-    SENTINEL,
-    DownloaderThread,
     DOWNLOADER_STARTED_EVT,
     DOWNLOADER_STOP_EVT,
+    SENTINEL,
+    DownloaderThread,
 )
 
 # TODO: use a queue per downloader instance and delete on kill
@@ -38,20 +41,51 @@ def update_status(sender):
 DOWNLOADER_STARTED_EVT.connect(update_status)
 
 
+pr = cProfile.Profile()
+pr.enable()
+
 sink = queue.Queue(DOWNLOAD_QUEUE_SIZE)
-downloader = DownloaderThread(youtube_link, sink_queue=sink, frame_skip=every)
+downloader = DownloaderThread(
+    youtube_link, sink_queue=sink, frame_skip=every, chunk_size=120
+)
 downloader.start()
 
-for frame in iter(sink.get, SENTINEL):
+for chunk in iter(sink.get, SENTINEL):
     if kill_button:
         DOWNLOADER_STOP_EVT.send(__name__)
         break
-    frame_display.image(np.array(frame, dtype=np.uint8), channels="BGR")
-    queue_size.progress(sink.qsize() / DOWNLOAD_QUEUE_SIZE)
-    queue_size_txt.markdown(f"Sink queue has {sink.qsize()}" f" unprocessed frames")
-    active_threads_count.markdown(
-        f"{len(downloader.active_downloaders())} active downloaders"
-    )
-    active_threads.markdown(
-        "\n".join([f"* {t.getName()}" for t in downloader.active_downloaders()])
-    )
+    for frame_info in chunk:
+        frame, ts = frame_info.values()
+        timestamp = time.strftime("%H:%M:%S", time.gmtime(ts / 1000))
+        frame_display.image(
+            np.array(frame, dtype=np.uint8), channels="BGR", caption=timestamp
+        )
+        queue_size.progress(sink.qsize() / DOWNLOAD_QUEUE_SIZE)
+        queue_size_txt.markdown(
+            f"Sink queue has {downloader.chunk_size * sink.qsize()}"
+            f" unprocessed frames"
+        )
+        active_threads_count.markdown(
+            f"{len(downloader.active_downloaders())} active downloaders"
+        )
+        active_threads.markdown(
+            "\n".join([f"* {t.getName()}" for t in downloader.active_downloaders()])
+        )
+
+queue_size.progress(sink.qsize() / DOWNLOAD_QUEUE_SIZE)
+queue_size_txt.markdown(
+    f"Sink queue has {downloader.chunk_size * sink.qsize()}" f" unprocessed frames"
+)
+active_threads_count.markdown(
+    f"{len(downloader.active_downloaders())} active downloaders"
+)
+active_threads.markdown(
+    "\n".join([f"* {t.getName()}" for t in downloader.active_downloaders()])
+)
+logger.debug(f"{threading.enumerate()}")
+
+pr.disable()
+s = io.StringIO()
+ps = pstats.Stats(pr, stream=s).sort_stats("cumulative")
+ps.print_stats()
+logger.debug(s.getvalue())
